cd615cf93600534fcba0180ad0bb5ebfc8c694cc
Xabi Barrero <ikcdx@plaiaundi.net>
2026-01-21 10:39:03 +0100

    Feat: Añadida la ruta a la pantalla de inicio

--- dev/backend/src/routes/web.php
+++ dev/backend/src/routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
 
 Route::get('/', function () {
-    return view('welcome');
+    $path = base_path('../frontend/index.html');
+
+    if (!File::exists($path)) {
+        return "Error: No se encuentra el archivo en " . $path;
+    }
+
+    return response()->file($path);
 });
 
 Route::get('/dashboard', function () {
@@ -16,5 +24,21 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('{any}', function ($filename) {
+    $path = base_path('../frontend/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+    
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header('Content-Type', $type);
+
+    return $response;
+})->where('any', '.*');
 
 require __DIR__.'/auth.php';
