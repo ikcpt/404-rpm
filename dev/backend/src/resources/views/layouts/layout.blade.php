f763b996a9576136f9357001328acea692a4df28
Eneko Errazquin <ikcpt@plaiaundi.net>
2026-02-02 09:54:53 +0100

    Fix: Footer layout

--- dev/backend/src/resources/views/layouts/layout.blade.php
+++ dev/backend/src/resources/views/layouts/layout.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="{{ asset('css/concesionario.css') }}">
     <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
     <link rel="stylesheet" href="{{ asset('css/buscador.css') }}">
-    @stack('styles')    
+    @stack('styles')
 </head>
 
 <body>
@@ -116,9 +116,28 @@
             <div class="columna-footer">
                 <h4>Síguenos</h4>
                 <div class="redes-sociales">
-                    <a href="#" class="icono-social"><svg viewBox="0 0 24 24" stroke="currentColor" fill="none">
+                    <a href="#" aria-label="Instagram" class="icono-social">
+                        <svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                            <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                        </svg>
+                    </a>
+                    <a href="#" aria-label="Facebook" class="icono-social">
+                        <svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
                             <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                        </svg></a>
+                        </svg>
+                    </a>
+                    <a href="#" aria-label="Twitter" class="icono-social">
+                        <svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path
+                                d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z">
+                            </path>
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
