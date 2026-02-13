67887f9153c5df55e8f89187d7b5f2387161d3ef
Xabi Barrero <ikcdx@plaiaundi.net>
2026-02-13 11:42:53 +0100

    Fix: Arreglar API reseñas

--- produccion/backend/src/app/Http/Controllers/ReviewController.php
+++ produccion/backend/src/app/Http/Controllers/ReviewController.php
@@ -12,54 +12,6 @@ class ReviewController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Review $review)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Review $review)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Review $review)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Review $review)
-    {
-        //
+        return response()->json(Review::with('user')->latest()->get());
     }
 }
